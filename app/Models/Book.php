5a4eb89a164464076a99dff8c08099db3dee8d2e
Shohan494 <shohan12224943@gmail.com>
2021-02-13 00:50:55 +0600

    allowing mass assignment on some fields

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,6 +9,8 @@ class Book extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['user_id', 'title', 'description'];
+
     public function user()
     {
       return $this->belongsTo(User::class);
