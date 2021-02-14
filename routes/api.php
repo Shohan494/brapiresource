811d1d5e0c032f80ce1ac1ba04c5e070b74d3168
Shohan494 <shohan12224943@gmail.com>
2021-02-15 01:29:24 +0600

    created resources and controllers

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
 Route::post('register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
+
+Route::apiResource('books', 'BookController');
+Route::post('books/{book}/ratings', 'RatingController@store');
